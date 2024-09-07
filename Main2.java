7d2872a8e92ffcb72ba734c9664c6c3b548d36dd
DzmitrySts <dimastepanenko89@gmail.com>
2024-09-07 20:43:43 +0300

    /

--- Main2.java
+++
@@ -1,8 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        Park park = new Park();
-        Park.Attraction super8 = park.new Attraction("Супер восемь", "09:00", "21:00", 5);
-
-        System.out.println(super8);
-    }
-}
