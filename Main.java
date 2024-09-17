cd27322142d91abb05abe427647aa015b4d997a4
DzmitrySts <dimastepanenko89@gmail.com>
2024-09-17 12:24:58 +0300

    Main

--- Main.java
+++ Main.java
@@ -31,15 +31,3 @@ public class Main {
         return sum;
     }
 }
-
-class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
-
-class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
